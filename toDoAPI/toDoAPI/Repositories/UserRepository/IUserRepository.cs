d0c8893218056e7147637da5322653c8d2b54a9b
Kavindu-Hasintha <mkavinduh@gmail.com>
2024-01-16 00:39:03 +0530

    UserRepository is added

---
+++ toDoAPI/toDoAPI/Repositories/UserRepository/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace toDoAPI.Repositories.UserRepository
+{
+    public interface IUserRepository
+    {
+        Task<bool> UserExist(int userId);
+        Task<User> GetUserAsync(int userId);
+    }
+}
---
+++ toDoAPI/toDoAPI/Repositories/UserRepository/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace toDoAPI.Repositories.UserRepository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUserAsync(int userId)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+        }
+
+        public async Task<bool> UserExist(int userId)
+        {
+            return await _context.Users.AnyAsync(x => x.Id == userId);
+        }
+    }
+}
